969f58c9510ecd52eda0a4a40073c96a4bc11d6b
Baraka Mulungula <barakamulungula@Interapts-MacBook-Pro-2.local>
2018-01-30 14:12:39 -0500

    Gussing game

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //Generate random number 1 to 50
+        //ask user to guess number
+        // if number too high tell them its too high
+        // if number too low tell them its too low
+        // if right tell them got the number right
+        // 5 trys to get number right
+        // when game ends given the option to end game or start new game
+
+    }
+}
